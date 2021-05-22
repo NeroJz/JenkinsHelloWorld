acbf6a7a0a63634e4b569e13bab99afe0213939f
KumHoe Lau <nerojz86@gmail.com>
2021-05-22 13:38:12 +0800

    Test Jenkins trigger build using webhook

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("-----***** Hello World & Welcome to Jenkins Training from GitHub*****-----")
+console.log("--------------------------------------------------------------------------")
+console.log("-----------        Trigger from webhook                         ----------")
+
